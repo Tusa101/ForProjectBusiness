5bedf6c17e26565755d97e3a1491113bfb032649
Tuska101 <amtuska@gmail.com>
2022-11-11 00:58:09 +0300

    BubbleSort project and solution were added

---
+++ BubbleSort.cpp
@@ -0,0 +1,51 @@
+﻿// BubbleSort.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+
+void BubbleSort(int*& arr_for_sort, const int& len)
+{
+    int temp;
+    for (int i=0; i < len; i++)
+        for (int j = 0; j < len-1; j++)
+        {
+            if (arr_for_sort[j]>arr_for_sort[j+1])
+            {
+                temp = arr_for_sort[j];
+                arr_for_sort[j] = arr_for_sort[j + 1];
+                arr_for_sort[j + 1] = temp;
+            }
+        }
+}
+void FillArr(int*& arr_for_sort,const int& len)
+{
+    cout << "Enter values for your arr:\n";
+    for (int i = 0; i < len; i++)
+    {
+        cout << i + 1 << " element:";
+        cin >> arr_for_sort[i];
+    }; 
+}
+void DisplayArr(int*& arr, const int& len)
+{
+    cout << "Sorted Array: \n";
+    for (int i = 0; i < len; i++)
+    {
+        cout << i + 1 << " element:";
+        cout << arr[i] << "\n";
+    };
+}
+
+int main()
+{
+    int len;
+    cout << "Enter array length: ";
+    cin >> len;
+    int* arr_for_sort = new int[len];
+    FillArr(arr_for_sort, len);
+    
+    BubbleSort(arr_for_sort, len);
+    DisplayArr(arr_for_sort, len);
+}
+
